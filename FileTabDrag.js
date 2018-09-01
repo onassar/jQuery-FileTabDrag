4e88277357e5ec1100ca59c6f9fa2a3d243ed549
onassar <onassar@gmail.com>
2018-09-01 19:53:25 -0400

    Directory uploading

--- FileTabDrag.js
+++ FileTabDrag.js
@@ -9,8 +9,42 @@
  *              'custom/drag/drop/files': function(event, files) {
  *              }
  *          });
+ * @see     https://stackoverflow.com/questions/12105900/fileentry-file-method-makes-datatransferitemlist-empty
+ * @see     https://developers.google.com/web/updates/2012/07/Drag-and-drop-a-folder-onto-Chrome-now-available
+ * @see     https://caniuse.com/#search=webkitDirectory
+ * @see     https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem/webkitGetAsEntry
+ * @see     https://stackoverflow.com/questions/11477412/html5-drag-and-drop-items-webkitgetasentry-method-doesnt-exist/11478372
+ * @see     https://stackoverflow.com/questions/5826286/how-do-i-use-google-chrome-11s-upload-folder-feature-in-my-own-code?lq=1
+ * @see     https://stackoverflow.com/questions/3590058/does-html5-allow-drag-drop-upload-of-folders-or-a-folder-tree
+ * @see     https://stackoverflow.com/questions/8856628/detecting-folders-directories-in-javascript-filelist-objects
+ * @see     https://developer.mozilla.org/en-US/docs/Web/API/FileSystemEntry/isDirectory
+ * @see     https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryReader/readEntries
+ * @see     https://stackoverflow.com/questions/45052875/how-to-convert-fileentry-to-standard-javascript-file-object-using-chrome-apps-fi
  */
 (function($) {
+
+    /**
+     * __allowDirectoryDropping
+     * 
+     * @access  private
+     * @return  Boolean (default: true)
+     */
+    var __allowDirectoryDropping = true;
+
+    /**
+     * __filenamesToIgnore
+     * 
+     * @access  private
+     * @return  Array
+     */
+    var __filenamesToIgnore = [
+        '.DS_Store',
+        '.git',
+        '.gitignore',
+        '.gitmodules',
+        '.htaccess',
+        '.svn'
+    ];
 
     /**
      * __contains
@@ -23,6 +57,128 @@
     var __contains = function(value, arr) {
         var index = $.inArray(value, arr);
         return index !== -1;
+    };
+
+    /**
+     * __containsDirectories
+     * 
+     * @access  private
+     * @param   jQuery event
+     * @return  Boolean
+     */
+    var __containsDirectories = function(event) {
+        if (__supportsDirectories() === false) {
+            return false;
+        }
+        var items = event.originalEvent.dataTransfer.items,
+            length = items.length,
+            item;
+        for (var i = 0; i < length; i++) {
+            var item = items[i];
+            if (__validValue(item.webkitGetAsEntry) === false) {
+                return false;
+            }
+            var entry = item.webkitGetAsEntry();
+            if (__validValue(entry.isDirectory) === false) {
+                return false;
+            }
+            if (entry.isDirectory === true) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    /**
+     * __convertDataTransferItemsToFiles
+     * 
+     * @access  private
+     * @param   jQuery event
+     * @param   Function callback
+     * @return  void
+     */
+    var __convertDataTransferItemsToFiles = function(event, callback) {
+        var items = event.originalEvent.dataTransfer.items,
+            entries = [],
+            entry;
+        for (var x = 0; x < items.length; x++) {
+            entry = items[x].webkitGetAsEntry();
+            entries.push(entry);
+        }
+        __convertEntriesToFiles(entries, callback);
+    };
+
+    /**
+     * __convertDirectoryEntryToFiles
+     * 
+     * @see     https://github.com/enyo/dropzone/issues/599
+     * @see     https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryReader/readEntries
+     * @access  private
+     * @param   DirectoryEntry|FileSystemDirectoryEntry directoryEntry
+     * @param   Function callback
+     * @param   undefined|Array files (default: [])
+     * @param   undefined|DirectoryReader reader (default: directoryEntry.createReader())
+     * @return  void
+     */
+    var __convertDirectoryEntryToFiles = function(directoryEntry, callback, entries, reader) {
+        entries = entries || [];
+        reader = reader || directoryEntry.createReader();
+        reader.readEntries(function(readEntries) {
+            if (readEntries.length === 0) {
+                __sortEntriesByName(entries);
+                __convertEntriesToFiles(entries, callback);
+            } else {
+                entries = entries.concat(readEntries);
+                __convertDirectoryEntryToFiles(directoryEntry, callback, entries, reader);
+            }
+        });
+    };
+
+    /**
+     * __convertEntriesToFiles
+     * 
+     * @access  private
+     * @param   Array entries
+     * @param   Function callback
+     * @param   undefined|Number index (default: 0)
+     * @param   undefined|Array files (default: [])
+     * @return  void
+     */
+    var __convertEntriesToFiles = function(entries, callback, index, files) {
+        index = index || 0;
+        files = files || [];
+        if (index === entries.length) {
+            callback(files);
+        } else {
+            var entry = entries[index];
+            if (entry.isFile === true) {
+                __convertFileEntryToFile(entry, function(file) {
+                    if (__ignoreFile(file) === false) {
+                        files.push(file);
+                    }
+                    __convertEntriesToFiles(entries, callback, index + 1, files);
+                });
+            } else {
+                __convertDirectoryEntryToFiles(entry, function(convertedFiles) {
+                    files = files.concat(convertedFiles);
+                    __convertEntriesToFiles(entries, callback, index + 1, files);
+                });
+            }
+        }
+    };
+
+    /**
+     * __convertFileEntryToFile
+     * 
+     * @access  private
+     * @param   FileEntry|FileSystemEntry fileEntry
+     * @param   Function callback
+     * @return  void
+     */
+    var __convertFileEntryToFile = function(fileEntry, callback) {
+        fileEntry.file(function(file) {
+            callback(file);
+        });
     };
 
     /**
@@ -44,7 +200,7 @@
      *          your server log may contain arbitrary requests unrelated to your
      *          app, which may just be attribute values of images (from other
      *          tabs or windows) that are being dragged in.
-     * @access  public
+     * @access  private
      * @param   Object event
      * @return  false|String
      */
@@ -85,35 +241,47 @@
     };
 
     /**
+     * __ignoreFile
+     * 
+     * @access  private
+     * @param   File file
+     * @return  Boolean
+     */
+    var __ignoreFile = function(file) {
+        var ignore = __contains(file.name, __filenamesToIgnore);
+        return ignore;
+    };
+
+    /**
      * __isFileDrag
      * 
      * @see     https://sentry.io/stencil/javascript/issues/573179290/?environment=prod
      *          https://i.imgur.com/JnYywfe.png
-     * @access  protected
+     * @access  private
      * @param   jQuery event
      * @return  Boolean
      */
     var __isFileDrag = function(event) {
-        if (event === undefined || event === null) {
+        if (__validValue(event) === false) {
             return false;
         }
         var originalEvent = event.originalEvent;
-        if (originalEvent === undefined || originalEvent === null) {
+        if (__validValue(originalEvent) === false) {
             return false;
         }
         var dataTransfer = originalEvent.dataTransfer;
-        if (dataTransfer === undefined || dataTransfer === null) {
+        if (__validValue(dataTransfer) === false) {
             return false;
         }
         var types = dataTransfer.types;
-        if (types === undefined || types === null) {
+        if (__validValue(types) === false) {
             return false;
         }
         var index, type;
         for (index in types) {
             type = types[index];
-            if (type.toLowerCase === undefined || type.toLowerCase === null) {
-                continue;
+            if (__validValue(type.toLowerCase) === false) {
+                return false;
             }
             if (type.toLowerCase() === 'files') {
                 return true;
@@ -136,15 +304,15 @@
      */
     var __isTabDrag = function(event) {
         var originalEvent = event.originalEvent;
-        if (originalEvent === undefined || originalEvent === null) {
+        if (__validValue(originalEvent) === false) {
             return false;
         }
         var dataTransfer = originalEvent.dataTransfer;
-        if (dataTransfer === undefined || dataTransfer === null) {
+        if (__validValue(dataTransfer) === false) {
             return false;
         }
         var files = dataTransfer.files;
-        if (files === undefined || files === null) {
+        if (__validValue(files) === false) {
             return false;
         }
         if (files.length > 0) {
@@ -221,9 +389,16 @@
      * @return  void
      */
     var __processFileDrop = function(event, $element) {
-        var files = event.originalEvent.dataTransfer.files,
-            eventType = 'custom/drag/drop/files';
-        $element.triggerHandler(eventType, [files]);
+        var eventType = 'custom/drag/drop/files';
+        if (__containsDirectories(event) === true) {
+            var callback = function(files) {
+                $element.triggerHandler(eventType, [files, true]);
+            }
+            __convertDataTransferItemsToFiles(event, callback);
+        } else {
+            var files = event.originalEvent.dataTransfer.files;
+            $element.triggerHandler(eventType, [files]);
+        }
     };
 
     /**
@@ -307,12 +482,60 @@
     };
 
     /**
+     * __sortEntriesByName
+     * 
+     * @see     https://stackoverflow.com/questions/10234515/html5-file-system-how-to-read-directories-using-directory-reader
+     * @see     https://stackoverflow.com/questions/2140627/javascript-case-insensitive-string-comparison
+     * @access  private
+     * @param   Array entries
+     * @return  void
+     */
+    var __sortEntriesByName = function(entries) {
+        entries.sort(function(a, b) {
+            var first = a.name.toLowerCase(),
+                second = b.name.toLowerCase();
+            return first < second ? -1 : second < first ? 1 : 0;
+        });
+    };
+
+    /**
+     * __supportsDirectories
+     * 
+     * @access  private
+     * @return  Boolean
+     */
+    var __supportsDirectories = function() {
+        return __allowDirectoryDropping;
+    };
+
+    /**
+     * __validValue
+     * 
+     * @access  private
+     * @param   mixed value
+     * @return  Boolean
+     */
+    var __validValue = function(value) {
+        if (value === undefined) {
+            return false;
+        }
+        if (value === null) {
+            return false;
+        }
+        return true;
+    };
+
+    /**
      * __construct
      * 
      * Operates as the constructor by calling the private __setupListeners
      * function, and returning itself to allow for chaining.
      */
-    $.fn.listenForFileAndTabDrag = function() {
+    $.fn.listenForFileAndTabDrag = function(options) {
+        options = options || [];
+        if (options.allowDirectoryDropping !== undefined) {
+            __allowDirectoryDropping = options.allowDirectoryDropping;
+        }
         __setupListeners(this);
         return this;
     };
